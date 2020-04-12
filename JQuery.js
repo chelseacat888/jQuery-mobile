35302545ede85a9b577d86c5a97213ff28d3b0a5
chelseacat888 <43200378+chelseacat888@users.noreply.github.com>
2020-04-11 21:24:44 -0500

    Add files via upload

---
+++ JQuery.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#down1").click(function(){
+      $('#page1').slideDown();
+    });
+
+
+    $("#up1").click(function(){
+        $('#page1').slideUp();
+      });
+
+  });
+
+  $(document).ready(function(){
+    $("#down2").click(function(){
+      $('#page2').slideDown();
+    });
+
+
+    $("#up2").click(function(){
+        $('#page2').slideUp();
+      });
+
+  });
+
+  $(document).ready(function(){
+    $("#down3").click(function(){
+      $('#page3').slideDown();
+    });
+
+
+    $("#up3").click(function(){
+        $('#page3').slideUp();
+      });
+
+  });
